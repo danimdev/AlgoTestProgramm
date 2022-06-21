f1806621723b210031901d2e33c9eb889e4a5b95
danimdev <106762674+danimdev@users.noreply.github.com>
2022-06-21 23:01:13 +0200

    Implementation of QuickSort Not Functioning yet

--- ConsoleAlgoTest/Program.cs
+++ ConsoleAlgoTest/Program.cs
@@ -5,24 +5,26 @@ Stopwatch sw = new();
 
 List<string> wordList = new List<string>();
 
-bool firstRun = false;
-
 GetWordList();
 
 while (true)
 {
-    for (int i = 0; i < 5; i++)
-    {
+    //for (int i = 0; i < 5; i++)
+    //{
 
-        //DotNetSort();
-        BubbleSort();
+    //DotNetSort();
+    //BubbleSort();
 
-        var rnd = new Random();
-        var randomized = wordList.OrderBy(item => rnd.Next());
 
-    }
-    Thread.Sleep(1000);
-    Console.Clear();
+    sw.Start();
+    quicksort(wordList, 0, wordList.Count - 1);
+
+    sw.Stop();
+    TimeSpan ts = sw.Elapsed;
+    Console.WriteLine(ts.TotalMilliseconds);
+
+    //var rnd = new Random();
+    //var randomized = wordList.OrderBy(item => rnd.Next());
 }
 
 //the standard dotnet algo
@@ -64,9 +66,45 @@ void BubbleSort()
 
     Console.WriteLine("Sort Time BubbleSort: " + ts.TotalMilliseconds);
 
-    sw.Restart();
+    sw.Reset();
     //Console.Read();
 }
+
+void quicksort(List<string> wordlist, int start, int end)
+{
+    if (start < end)
+    {
+        int pivotIndex = partition(wordlist, start, end);
+        quicksort(wordlist, start, pivotIndex - 1);
+        quicksort(wordlist, pivotIndex + 1, end);
+    }
+}
+
+int partition(List<string> arr, int start, int end)
+{
+    int pivot = end;
+    int i = start, j = end;
+    string temp;
+    while (i < j)
+    {
+        while (i < end && string.Compare(arr[i], arr[pivot]) < 0)
+            i++;
+        while (j > start && string.Compare(arr[j], arr[pivot]) > 0)
+            j--;
+
+        if (i < j)
+        {
+            temp = arr[i];
+            arr[i] = arr[j];
+            arr[j] = temp;
+        }
+    }
+    temp = arr[pivot];
+    arr[pivot] = arr[j];
+    arr[j] = temp;
+    return j;
+}
+
 
 void GetWordList()
 {
